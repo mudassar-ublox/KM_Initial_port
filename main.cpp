ff3b0455a83127f772a986b397e81629b52b36e1
Mudassar Hussain <mudassar.hussain@u-blox.com>
2018-01-22 12:30:58 +0500

    SPI Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,48 @@
-
 #include "mbed.h"
 #include "rtos.h"
 
-Mutex mutex;
-
-void Th_print_1(const char* name, int state) {
-	mutex.lock();
-    printf("Thread 1\n\r");
-    mutex.unlock();
-}
-
-void Th_print_2(const char* name, int state) {
-	mutex.lock();
-    printf("Thread 1\n\r");
-    mutex.unlock();
-}
-
-void test_thread_1(void const *args) {
-    while (true) {
-    	Th_print_1((const char*)args, 0);
-        Thread::wait(1000);
-    }
-}
-
-void test_thread_2(void const *args) {
-    while (true) {
-    	Th_print_2((const char*)args, 0);
-        Thread::wait(1000);
-    }
-}
+SPI spi(SPI1_MOSI, SPI1_MISO, SPI1_CLK, SPI1_CS); // mosi, miso, sclk
+DigitalOut cs(SPI1_CS);
 
 int main() {
-    Thread t1(test_thread_1);
-    Thread t2(test_thread_2);
+    // Chip must be deselected
 
-    test_thread_1((void *)"Th 1");
-    test_thread_2((void *)"Th 2");
+    int x = 0, y = 0;
+
+    printf("Starting Master. . .\r\n");
+    cs = 1;
+
+    // Setup the spi for 8 bit data, high steady state clock,
+    // second edge capture, with a 1MHz clock rate
+    //spi.format(8,0);
+    //spi.frequency(1000000);
+
+    // Select the device by seting chip select low
+    cs = 0;
+
+    // Send 0x8f, the command to read the WHOAMI register
+    //spi.write(0x8F);
+
+    // Send a dummy byte to receive the contents of the WHOAMI register
+    //int whoami = spi.write(0x00);
+    //printf("WHOAMI register = 0x%X\n", whoami);
+
+	//spi.write(0x00);
+    // Deselect the device
+    //cs = 1;
+    while(1){
+		cs = 0;
+        x++;
+        printf("Data Send = %d\r\n", x);
+        y = spi.write(x);
+        y = spi.write(0x00);
+        printf("Data Received = %d\r\n", y);
+		cs = 1;
+        //wait_ms(1000);
+        y = 0;
+
+		if(x >= 255){
+			x = 0;
+		}
+    }
 }
