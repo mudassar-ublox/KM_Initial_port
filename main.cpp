bce56fdf1dac8ff8372cab31cbcd5d89b689ba25
Mudassar Hussain <mudassar.hussain@u-blox.com>
2018-01-22 12:27:24 +0500

    GPIO Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,60 @@
-
 #include "mbed.h"
 #include "rtos.h"
 
-Mutex mutex;
 
-void Th_print_1(const char* name, int state) {
-	mutex.lock();
-    printf("Thread 1\n\r");
-    mutex.unlock();
+DigitalOut led0(LED0);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+DigitalOut led5(LED5);
+DigitalOut led6(LED6);
+
+Serial pc(UART1_TX, UART1_RX);
+
+bool gpio_check = FALSE;
+
+void gpio_led_toggle(void) {
+
+
+	led0 != led0;
+	led1 != led1;
+	led2 != led2;
+	led3 != led3;
+	led4 != led4;
+	led5 != led5;
+	led6 != led6;
+		
+    if(!gpio_check)
+    {
+    	gpio_check = TRUE;
+        pc.printf("LED's ON\n");
+    }
+    else
+    {
+    	gpio_check = FALSE;
+        pc.printf("LED's OFF\n\n");
+    }
 }
 
-void Th_print_2(const char* name, int state) {
-	mutex.lock();
-    printf("Thread 1\n\r");
-    mutex.unlock();
-}
 
 void test_thread_1(void const *args) {
-    while (true) {
-    	Th_print_1((const char*)args, 0);
-        Thread::wait(1000);
-    }
+
+	while(true){
+		gpio_led_toggle();
+		Thread::wait(10);
+	}
 }
 
-void test_thread_2(void const *args) {
-    while (true) {
-    	Th_print_2((const char*)args, 0);
-        Thread::wait(1000);
-    }
-}
 
 int main() {
-    Thread t1(test_thread_1);
-    Thread t2(test_thread_2);
+    Thread t1(osPriorityHigh);
 
-    test_thread_1((void *)"Th 1");
-    test_thread_2((void *)"Th 2");
+    pc.printf("GPIO DEMO STARTS ...\r\n");
+
+    t1.start(callback(test_thread_1, (void *)"LED's Thread"));
+
+    while(1);
+
 }
+
